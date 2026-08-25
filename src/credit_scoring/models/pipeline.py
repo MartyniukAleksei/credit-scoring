4db25d017a8e518e9f49137469146d3f9733c1e2
Aleksei <m0982110323@gmail.com>
2026-08-25 23:44:51 +0300

    logreg and lightgbm with optuna implemetation

--- src/credit_scoring/models/pipeline.py
+++ src/credit_scoring/models/pipeline.py
@@ -9,6 +9,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score, average_precision_score
 from lightgbm import LGBMClassifier
 
 def load_data() -> tuple[pd.DataFrame, pd.Series]:
@@ -63,21 +65,31 @@ def build_lgbm_pipeline() -> Pipeline:
     
     return lgbm_pipeline
 
+def split_data(X: pd.DataFrame, y: pd.Series) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    return train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+
 if __name__ == '__main__':
     X, y = load_data()
+    X_train, X_test, y_train, y_test = split_data(X, y)
     
     logred = build_logreg_pipeline()
-    logred.fit(X, y)
-    preds = logred.predict_proba(X)[:, 1]
-    assert len(preds) == len(X), "the number of predictions have to be equal to number of rows"
-    assert not pd.Series(preds).isna().any(), "не должно быть NaN в предсказаниях"
-    assert ((preds >= 0) & (preds <= 1)).all(), "prohabilities have to be between [0,1]"
-    print(f"LogReg: fit OK, mean prediction = {preds.mean():.3f} (baseline ~0.07)")
+    logred.fit(X_train, y_train)
+    for name, X_type, y_type in [('Train', X_train, y_train), ('Test', X_test, y_test)]:
+        preds = logred.predict_proba(X_type)[:, 1]
+        auc = roc_auc_score(y_type, preds)
+        pr_auc = average_precision_score(y_type, preds)
+        assert len(preds) == len(X_type), "the number of predictions have to be equal to number of rows"
+        assert not pd.Series(preds).isna().any(), "не должно быть NaN в предсказаниях"
+        assert ((preds >= 0) & (preds <= 1)).all(), "prohabilities have to be between [0,1]"
+        print(f"LogReg {name}: fit OK, ROC-AUC={auc} | PR_AUC={pr_auc} | mean prediction = {preds.mean():.3f} (baseline ~0.07)")
     
     lgbm = build_lgbm_pipeline()
-    lgbm.fit(X, y)
-    preds_lgbm = lgbm.predict_proba(X)[:, 1]
-    assert len(preds_lgbm) == len(X)
-    assert not pd.Series(preds_lgbm).isna().any()
-    assert ((preds_lgbm >= 0) & (preds_lgbm <= 1)).all()
-    print(f"LightGBM: fit OK, mean prediction = {preds_lgbm.mean():.3f} (baseline ~0.07)")
+    lgbm.fit(X_train, y_train)
+    for name, X_type, y_type in [('Train', X_train, y_train), ('Test', X_test, y_test)]:
+        preds_lgbm = lgbm.predict_proba(X_type)[:, 1]
+        auc = roc_auc_score(y_type, preds_lgbm)
+        pr_auc = average_precision_score(y_type, preds_lgbm)
+        assert len(preds_lgbm) == len(X_type)
+        assert not pd.Series(preds_lgbm).isna().any()
+        assert ((preds_lgbm >= 0) & (preds_lgbm <= 1)).all()
+        print(f"LightGBM {name}: fit OK, ROC-AUC={auc} | PR-AUC={pr_auc} | mean prediction = {preds_lgbm.mean():.3f} (baseline ~0.07)")
---
+++ src/credit_scoring/models/tune.py
@@ -0,0 +1,53 @@
+import optuna
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import roc_auc_score, average_precision_score
+from lightgbm import LGBMClassifier
+from credit_scoring.features.clipper import Clipper
+from credit_scoring.models.pipeline import load_data, split_data
+
+def objective(trial: optuna.Trial, X: pd.DataFrame, y: pd.Series) -> float:
+    params = {
+        "num_leaves": trial.suggest_int("num_leaves", 8, 128),
+        "max_depth": trial.suggest_int("max_depth", 3, 12),
+        "min_child_samples": trial. suggest_int("min_child_samples", 10, 200),
+        "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.3, log=True),
+        "n_estimators": trial.suggest_int("n_estimators", 50, 500),
+        "reg_alpha": trial.suggest_float("reg_alpha", 1e-8, 10.0, log=True),
+        "reg_lambda": trial.suggest_float("reg_lambda", 1e-8, 10.0, log=True),
+    }
+    
+    pipeline = Pipeline([
+        ("clip", Clipper(columns=["DebtRatio", "RevolvingUtilizationOfUnsecuredLines"])),
+        ("model", LGBMClassifier(**params, random_state=42, verbosity=-1))
+    ]
+    )
+    
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    scores = cross_val_score(pipeline, X, y, cv=cv, scoring='average_precision')
+    return scores.mean()
+
+if __name__ == "__main__":
+    X, y = load_data()
+    X_train, X_test, y_train, y_test = split_data(X, y)
+    
+    study = optuna.create_study(direction="maximize")
+    study.optimize(lambda trial: objective(trial, X_train, y_train), n_trials=30)
+    
+    print("Best PR-AUC (CV):", study.best_value)
+    print("Best params:", study.best_params)
+    
+    final_lgbm = Pipeline([
+            ("clip", Clipper(columns=["DebtRatio", "RevolvingUtilizationOfUnsecuredLines"])),
+            ("model", LGBMClassifier(**study.best_params, random_state=42, verbosity=-1))
+        ]
+        )
+    final_lgbm.fit(X_train, y_train)
+    
+    for name, X_part, y_part in [('training', X_train, y_train), ('test', X_test, y_test)]:
+        pred = final_lgbm.predict_proba(X_part)[:,1]
+        roc = roc_auc_score(y_part, pred)
+        pr_auc = average_precision_score(y_part, pred)
+        print(f"Model {name}: fits OK | ROC-AUC={roc:.3f} | PR-AUC={pr_auc:.3f}")
+    
